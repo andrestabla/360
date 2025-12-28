69090ff99c40813113db7af1130f715986a0e745
Andrés Tabla <andrestabla@gmail.com>
2025-12-28 16:05:25 -0500

    Fix build error: Add faviconUrl to PlatformSettings type in data.ts

--- lib/data.ts
+++ lib/data.ts
@@ -399,6 +399,8 @@ export interface PlatformSettings {
     loginBackgroundColor?: string;
     loginBackgroundImage?: string;
     logoUrl?: string;
+    faviconUrl?: string;
+    [key: string]: any;
   };
 }
 
